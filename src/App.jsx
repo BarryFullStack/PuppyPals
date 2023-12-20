d0cede241da29ffd6c6a72b7dff3ea1c92408128
Lisa Barry <lisa.barry3@gmail.com>
2023-12-19 20:57:48 -0600

    data with useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-
+import {puppyList} from './data.js'
+import { useState } from 'react'
 
 function App() {
-  
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log("puppyList:", puppyList);
 
   return (
     <>
-      <div>
-        
+      <div className="App">
+      { 
+        puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>;
+        })
+      }
       </div>
     </>
-  )
+  );
 }
 
 export default App
